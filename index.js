570ea67f7c9f9757a0bd23b5f39906faf43cd64f
Jefferson Braz <65771808+brazjeffe01@users.noreply.github.com>
2023-12-30 13:43:52 -0300

    Refatorando o código do projeto.
    Adicionando dados fixos como entrada de dados.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const read = require('readline-sync')
-
 const apr = () => {
     console.log('-----------------------------------------\n----- BVA (BOUNDARY VALUE ANALYSIS) -----\n----------------------------------------- ')
-    console.log('Esse é uma técnica de testes onde podemos validar quais valores são e não são aceitos dentro de um limite, com o intúito de aumentar a cobertura e diminiur a quantidade de dados.')
+    console.log('Esse é uma técnica de testes onde podemos validar quais valores são e não são aceitos dentro de um limite, com o intúito de aumentar a cobertura e diminiur a quantidade de dados.\n')
     exp()
 }
 
 const exp = () => {
     console.log('Exemplo: Dentro de uma range de valores entre 200 e 300, validamos quais são seus valores limites positivos e negativos, sempre utilizando a seguinte fórmula:')
-    console.log('Valor Limite Inicial - 1, Valor Limite Inicial, Valor Limite Inicial +1, Valor Limite Final -1, Valor Limite Final, Valor Limite Final +1')
+    console.log('Valor Limite Inicial - 1, Valor Limite Inicial, Valor Limite Inicial +1, Valor Limite Final -1, Valor Limite Final, Valor Limite Final +1\n')
 }
 
 const bva = (vi, vf) => {
@@ -27,19 +25,12 @@ const bva = (vi, vf) => {
         const vfPlus = +vf + 1
         const valores = [viMinus, vi, viPlus,vfMinus, vf, vfPlus]
         
-        console.log(valores)
+        console.log('Valores de entrada utilizados: Valor inicial: 200, Valor Final: 500.\nResultado: ' + valores)
 
     }
 }
 
 apr()
 
-const valorInicial = read.questionInt('Digite o valor inicial: ', {
-    limitMessage: 'Por favor, digite um numero valido.'
-})
-const valorFinal = read.questionInt('Digite o valor final: ', {
-    limitMessage: 'Por favor, digite um numero valido.'
-})
-
-bva(valorInicial,valorFinal)
+bva(200,500)
 
